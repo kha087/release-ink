ad4292c58ba16d73d556635bdf1eaa9537a32362
Kenneth Haugen <kha087@politiet.no>
2026-03-13 18:37:42 +0100

    clean up.

--- src/main/java/no/politiet/hendelse/appteam/release/ink/Main.java
+++
@@ -1,7 +0,0 @@
-package no.politiet.hendelse.appteam.release.ink;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("The advance complex release increment program.");
-	}
-}	
